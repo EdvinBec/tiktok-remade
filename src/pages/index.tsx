6c4a76a3f1b7e5d93c9b552751c3682482e203be
EDVIN\edvin <edvinbecic16@outlook.com>
2023-04-04 18:55:07 +0200

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>TikTok - Remade - Edvin</title>
+        <meta
+          name="description"
+          content="Clone of the popular Social Media App - TikTok"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
