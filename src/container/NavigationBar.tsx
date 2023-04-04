289eb23bb8a5a24a84cff7f40cccf28749a99e1a
EDVIN\edvin <edvinbecic16@outlook.com>
2023-04-04 22:32:50 +0200

    add logo to navigation bar

--- src/container/NavigationBar.tsx
+++ src/container/NavigationBar.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import TikTokLogo from "../assets/img/TikTokLogo.svg";
+import Image from "next/image";
 
 type Props = {};
 
 const NavigationBar = (props: Props) => {
-  return <div>NavigationBar</div>;
+  return (
+    <nav>
+      <button className="homeLogoButton">
+        <Image src={TikTokLogo} alt="TikTok" className="fullSizeLogo" />
+      </button>
+    </nav>
+  );
 };
 
 export default NavigationBar;
