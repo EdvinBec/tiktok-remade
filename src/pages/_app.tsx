83974fac2769de754e0858207dfb51384f983c05
EDVIN\edvin <edvinbecic16@outlook.com>
2023-04-05 23:01:47 +0200

    add layout component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,14 @@ import "@/styles/SearchBar.scss";
 import "@/styles/Button.scss";
 
 import type { AppProps } from "next/app";
+import Layout from "@/components/Layout";
+import NavigationBar from "@/container/NavigationBar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <NavigationBar />
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
