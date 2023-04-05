1dafd97860ad63fc086d6607602cae9e86901287
EDVIN\edvin <edvinbecic16@outlook.com>
2023-04-05 23:19:03 +0200

    fix Head component position

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,21 @@
+import Head from "next/head";
 import React from "react";
 
 const Layout = ({ children }: any) => {
-  return <div className="layout">{children}</div>;
+  return (
+    <div className="layout">
+      <Head>
+        <title>TikTok - Remade - Edvin</title>
+        <meta
+          name="description"
+          content="Clone of the popular Social Media App - TikTok"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,6 @@
-import NavigationBar from "@/container/NavigationBar";
-import Head from "next/head";
-
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>TikTok - Remade - Edvin</title>
-        <meta
-          name="description"
-          content="Clone of the popular Social Media App - TikTok"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <main></main>
     </>
   );
